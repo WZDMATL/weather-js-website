7170efc96008a6c03297dd7a189afe5751119d4e
WZDMATL <82984756+WZDMATL@users.noreply.github.com>
2021-05-24 21:32:34 -0400

    Updated index.js to program the weather API
    Corrected the previous code

--- index.js
+++ index.js
@@ -24,14 +24,45 @@ if (minutes < 10) {
 
 h1Details.innerHTML = `${day} ${hour}:${minutes}`;
 
-function searchNewCity(event) {
-  event.preventDefault();
-  let cityName = document.querySelector("#city");
-  let cityInput = document.querySelector("#searchinput");
-  cityName.innerHTML = `${cityInput.value}`;
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+
+  document.querySelector("#tempNumber").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `${response.data.wind.speed} km/h`;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
-let searchCity = document.querySelector("#search");
-searchCity.addEventListener("submit", searchNewCity);
+
+function searchCity(city) {
+  let apiKey = "60c52e9588bed9478b2ad668f1ee55d2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchinput").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "60c52e9588bed9478b2ad668f1ee55d2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
 
 function convertC(event) {
   event.preventDefault();
@@ -43,56 +74,6 @@ function convertF(event) {
   let tempF = document.querySelector("#tempNumber");
   tempF.innerHTML = 55;
 }
-let cLink = document.querySelector("#celTemp");
-cLink.addEventListener("click", convertC);
-let fLink = document.querySelector("#fahTemp");
-fLink.addEventListener("click", convertF);
-
-let apiKey = "60c52e9588bed9478b2ad668f1ee55d2";
-console.log(apiKey);
-
-function displayWeatherCondition(response) {
-  let apiKey = "16349f381dfaa3ea072aee8c62fc1b3b";
-  let units = "metric";
-  let SearchResult = document.querySelector("#searchinput").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${SearchResult}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#tempNumber").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-}
-
-function SearchCity(city) {
-  let apiKey = `9b17ad7a8e9bb4a84d31521ba60a3369`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#searchinput").value;
-  SearchCity(city);
-}
-
-function retrievePosition(position) {
-  let apiKey = "9b17ad7a8e9bb4a84d31521ba60a3369";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(retrievePosition);
-}
-
-let citynew = document.querySelector("#search");
-citynew.addEventListener("submit", SearchCity);
 
 let searchForm = document.querySelector("#search");
 searchForm.addEventListener("submit", handleSubmit);
